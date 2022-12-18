df388e7455fecb9bcf4a81e0d9f93856b992c678
Georg <georghome1@gmail.com>
2022-12-18 19:55:08 +0300

    Hello User

---
+++ Exampe002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Приглашение к вводу
+Console.Write("Введите ваше имя ");
+// Ввод имени пользователя
+string username = Console.ReadLine();
+ // Вывод приветсвия в консоль
+ Console.Write("Привет, ");
+ Console.Write(username);
